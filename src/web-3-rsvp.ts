7a26d6ff4471b18575a7dd1ea4ce743341ba5c4f
Jordan <jordanrob709@gmail.com>
2022-08-06 17:44:07 +0300

    added metadata image path code to handleNewEventCreated fuction in mappings

--- src/web-3-rsvp.ts
+++ src/web-3-rsvp.ts
@@ -60,21 +60,35 @@ export function handleNewEventCreated(event: NewEventCreated): void {
         const name = value.get("name");
         const description = value.get("description");
         const link = value.get("link");
-
+        const imagePath = value.get("image");
+    
         if (name) {
           newEvent.name = name.toString();
         }
-
+    
         if (description) {
           newEvent.description = description.toString();
         }
-
+    
         if (link) {
           newEvent.link = link.toString();
         }
+    
+        if (imagePath) {
+          const imageURL =
+            "https://ipfs.io/ipfs/" +
+            event.params.eventDataCID +
+            imagePath.toString();
+          newEvent.imageURL = imageURL;
+        } else {
+          // return fallback image if no imagePath
+          const fallbackURL =
+            "https://ipfs.io/ipfs/bafybeibssbrlptcefbqfh4vpw2wlmqfj2kgxt3nil4yujxbmdznau3t5wi/event.png";
+          newEvent.imageURL = fallbackURL;
+        }
       }
     }
-
+    
     newEvent.save();
   }
 }
